141cbb9634d70dd048a74e537021b6e326480d56
Victor Omboke <victoromboke@gmail.com>
2023-05-29 12:30:21 -0700

    Test addBookToLibrary() function with console

--- library.js
+++ library.js
@@ -5,4 +5,7 @@ function Book() {}
 function addBookToLibrary() {
   let askUser = prompt("Please enter your favorite book!", "Harry Potter");
   myLibrary.push(askUser);
+  console.log(myLibrary);
 }
+
+addBookToLibrary();
