c9bdda366b713a7b790118774d12f4a52ea44a7d
Victor Omboke <victoromboke@gmail.com>
2023-05-30 14:23:36 -0700

    Add to Book constructor and myLibrary array, create new function to display array elements individually

--- library.js
+++ library.js
@@ -1,11 +1,32 @@
-let myLibrary = [];
+let myLibrary = [
+  "Attack On Titan",
+  "The Hunger Games",
+  "Thinking, Fast and Slow",
+];
 
-function Book() {}
+function Book(title, author, pages, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
+}
 
 function addBookToLibrary() {
   let askUser = prompt("Please enter your favorite book!", "Harry Potter");
   myLibrary.push(askUser);
-  console.log(myLibrary);
 }
 
 addBookToLibrary();
+
+const display = document.querySelector("#main-content");
+
+function displayBook() {
+  myLibrary.forEach((book) => {
+    let newBook = document.createElement("div");
+    newBook.classList.add("card");
+    newBook.innerHTML = book;
+    display.appendChild(newBook);
+  });
+}
+
+displayBook();
